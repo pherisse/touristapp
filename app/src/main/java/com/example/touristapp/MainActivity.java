b867719509562a7c4a06a50d4e3e19713acd94a4
pherisse <pherisse@accengage.com>
2019-01-18 18:08:07 +0100

    firesbase implementation

--- app/src/main/java/com/example/touristapp/MainActivity.java
+++ app/src/main/java/com/example/touristapp/MainActivity.java
@@ -9,17 +9,19 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.touristapp.EXTRA_MESSAGE";
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
